a1ffb4a0a7301a8c7c3dd8a7133880217c6ed429
Olade <oladejiadekunle931@gmail.com>
2025-05-02 10:06:16 +0100

    git init

--- src/Pratice/RandomInteger.java
+++ src/Pratice/RandomInteger.java
@@ -4,14 +4,6 @@ import java.security.SecureRandom;
 public class RandomInteger {
     public static void main(String[] args) {
         SecureRandom random = new SecureRandom(); // An instance of secure random used to generate random numbers
-
-        for(int counter = 1; counter <= 20; counter++){
-            int face = 1 + random.nextInt(6);
-            System.out.printf("%d", face);
-
-            if(counter % 5 == 0){
-                System.out.println();
-            }
-        }
+        System.out.println(random.nextInt(6));
     }
 }
